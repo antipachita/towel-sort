1086692c2957ead677a129734e239d3b8d64a131
antipachita <nice.strogonov@inbox.ru>
2022-02-12 13:52:34 +0300

    feat: add js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix) {
+        return [];
+    }
+    let numArr = [];
+    for (let i =0; i<matrix.length; i++) {
+        if (i%2 === 0) {
+          numArr.push(matrix[i])
+        } else {
+          numArr.push(matrix[i].reverse())
+        }
+      }   
+     
+      return numArr.flat()
+    }
